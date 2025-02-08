01c583724d3922c63e7201111911e55c2881e300
imgopz <rrgkmech@gmail.com>
2025-02-08 10:47:41 +0530

    env loading is missing

--- patterns/workflows/1-introduction/1-basic.py
+++ patterns/workflows/1-introduction/1-basic.py
@@ -1,9 +1,13 @@
 import os
-
+from dotenv import load_dotenv
 from openai import OpenAI
+
+# Load environment variables from .env file
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+print(client)
 
 completion = client.chat.completions.create(
     model="gpt-4o",
